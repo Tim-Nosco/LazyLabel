f32f5f1a574511911911c25bce427d3fff296f55
tim-nosco <tim.nosco@gmail.com>
2017-10-20 17:07:51 -0400

    no-work

--- label.py
+++ label.py
@@ -1,51 +1,36 @@
 from z3 import *
 import logging
 
-def ABS_SUB(x,y):
-	return If(x>y,x-y,y-x)
-
 class Labeler:
-	def __init__(self,tree):
+	def __init__(self,n):
 		logging.debug("Setting up labeler")
-		self.s = Solver()
-		self.orig = tree
-		self.n = len(tree)
+		self.n = n
 		logging.debug("Creating edges and verticies")
 		self.edges = []
 		self.vert = []
 		self.clauses = []
-		for i in range(1,n+1):
-			e = Int("e{}".format(i))
-			self.edges.append(e)
-			self.clauses.append(e >= 1)
-			self.clauses.append(e <= n)
 		for i in range(n+1):
 			v = Int("v{}".format(i))
 			self.vert.append(v)
 			self.clauses.append(v >= 0)
 			self.clauses.append(v <= n)
+		for i in range(n):
+			e = Int("e{}".format(i))
+			self.edges.append(e)
+			self.clauses.append(e >= 1)
+			self.clauses.append(e <= n)
 		self.clauses += [Distinct(*self.edges),
-				Distinct(*self.vert)]
+						 Distinct(*self.vert)]
 		self.setup()
 	def setup(self):
 		self.s.reset()
 		self.s.insert(*self.clauses)
-		#a label is an ordering of edge labels
-		self.label = [i for i in range(1,n+1)]
-	def assertLabel(self):
-		for i,e in enumerate(self.label):
-			v1,v2 = [self.vert[v] for v in self.orig[i]]
-			edge = self.edges[e]
-			self.s.add(edge==ABS_SUB(v1,v2))
 	def reject(self):
 		logging.debug("Checking with solver...")
-		if not s.check():
-			return True
-		else:
-			#TODO custom checks
-			#if the check fails, append to self.clauses
-			# set restart flag, return True
-			return False
-	
-
-
+		r = s.check()
+		logging.debug("Solver: {}",r)
+		return r==sat
+	def connect(self):
+		#TODO assert that an edge is the connection of two verticies
+		# hope that the solver returns unsat (a labeling does not exist)
+		pass
